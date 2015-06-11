21d0d42db52b7b8bd3313d6e753d8ed723784b41
Vladimir Panteleev <vladimir@thecybershadow.net>
2015-06-11 11:35:44 +0000

    web: Diffs and stuff

--- webserver.d
+++ webserver.d
@@ -1,5 +1,6 @@
 import std.algorithm;
 import std.array;
+import std.conv;
 import std.exception;
 import std.file;
 import std.format;
@@ -12,9 +13,12 @@ import ae.net.http.common;
 import ae.net.http.responseex;
 import ae.net.http.server;
 import ae.net.shutdown;
+import ae.sys.d.cache;
+import ae.sys.git;
 import ae.sys.log;
 import ae.utils.exception;
 import ae.utils.meta;
+import ae.utils.mime;
 import ae.utils.regex;
 import ae.utils.sini;
 import ae.utils.textout;
@@ -28,8 +32,10 @@ struct Config
 immutable Config config;
 
 StringBuffer html;
-
 Logger log;
+
+Repository cache;
+Repository.ObjectReader objectReader;
 
 void onRequest(HttpRequest request, HttpServerConnection conn)
 {
@@ -50,6 +56,7 @@ HttpResponse handleRequest(HttpRequest request, HttpServerConnection conn)
 		auto path = pathStr[1..$].split("/");
 		if (!path.length) path = [""];
 
+		pathSwitch:
 		switch (path[0])
 		{
 			case "":
@@ -58,11 +65,65 @@ HttpResponse handleRequest(HttpRequest request, HttpServerConnection conn)
 				break;
 			case "results":
 				title = "Test result";
-				enforce(path.length >= 3);
-				if (path.length == 4 && path[3] == "build.log")
-					return response.serveFile(pathStr[1..$], "");
-				showResult(path[1], path[2]);
+				enforce(path.length > 3, "Bad path");
+				enforce(path[1].match(re!`^[0-9a-f]{40}$`), "Bad base commit");
+				enforce(path[2].match(re!`^[0-9a-f]{40}$`) || path[2] == "!base", "Bad pull commit");
+
+				auto testDir = "results/%s/%s/".format(path[1], path[2]);
+				enforce(testDir.exists, "No such commit");
+
+				auto action = path[3];
+				switch (action)
+				{
+					case "":
+						showResult(testDir);
+						break;
+					case "build.log":
+						return response.serveFile(pathStr[1..$], "");
+					case "file":
+					{
+						auto buildID = readText(testDir ~ "buildid.txt");
+						return response.redirect("/artifact/" ~ buildID ~ "/" ~ path[4..$].join("/"));
+					}
+					case "diff":
+					{
+						auto buildID = readText(testDir ~ "buildid.txt");
+						auto baseBuildID = readText(testDir ~ "../!base/buildid.txt");
+						return response.redirect("/diff/" ~ baseBuildID ~ "/" ~ buildID ~ "/" ~ path[4..$].join("/"));
+					}
+					default:
+						throw new Exception("Unknown action");
+				}
 				break;
+			case "artifact":
+			{
+				enforce(path.length >= 2, "Bad path");
+				auto refName = GitCache.refPrefix ~ path[1];
+				auto commitObject = objectReader.read(refName);
+				auto obj = objectReader.read(commitObject.parseCommit().tree);
+				foreach (dirName; path[2..$])
+				{
+					auto tree = obj.parseTree();
+					if (dirName == "")
+					{
+						title = "Artifact storage directory listing";
+						showDirListing(tree);
+						break pathSwitch;
+					}
+					auto index = tree.countUntil!(entry => entry.name == dirName);
+					enforce(index >= 0, "Name not in tree: " ~ dirName);
+					obj = objectReader.read(tree[index].hash);
+				}
+				enforce(obj.type == "blob", "Invalid object type");
+				return response.serveData(Data(obj.data), guessMime(path[$-1]));
+			}
+			case "diff":
+			{
+				enforce(path.length >= 4, "Bad path");
+				auto refA = GitCache.refPrefix ~ path[1];
+				auto refB = GitCache.refPrefix ~ path[2];
+				return response.serveText(cache.query(["diff", refA, refB, "--", path[3..$].join("/")]));
+			}
 			case "static":
 				return response.serveFile(pathStr[1..$], "web/");
 			case "robots.txt":
@@ -89,13 +150,26 @@ void showIndex()
 	html.put("This is the DAutoTest web service.");
 }
 
-void showResult(string baseCommit, string pullCommit)
+void showDirListing(GitObject.TreeEntry[] entries)
 {
-	enforce(baseCommit.match(re!`^[0-9a-f]{40}$`), "Bad base commit");
-	enforce(pullCommit.match(re!`^[0-9a-f]{40}$`) || pullCommit == "!base", "Bad pull commit");
-	auto testDir = "results/%s/%s/".format(baseCommit, pullCommit);
-	enforce(testDir.exists, "No such commit");
+	html.put(
+		`<ul class="dirlist">`
+		`<li>       <a href="../">..</a></li>`
+	);
+	foreach (entry; entries)
+	{
+		auto name = encodeEntities(entry.name) ~ (entry.mode & octal!40000 ? `/` : ``);
+		html.put(
+			`<li>`, "%06o".format(entry.mode), ` <a href="`, name, `">`, name, `</a></li>`
+		);
+	}
+	html.put(
+		`</ul>`
+	);
+}
 
+void showResult(string testDir)
+{
 	string tryReadText(string fileName, string def = null) { return fileName.exists ? fileName.readText : def; }
 
 	auto result = tryReadText(testDir ~ "result.txt", "Unknown\n(unknown)").splitLines();
@@ -106,10 +180,61 @@ void showResult(string baseCommit, string pullCommit)
 		`<tr><td>Component</td><td>`, info[0], `</td></tr>`
 		`<tr><td>Pull request</td><td><a href="`, info[2], `">#`, info[1], `</a></td></tr>`
 		`<tr><td>Status</td><td>`, result[0], `</td></tr>`
-		`<tr><td>Description</td><td>`, result[1], `</td></tr>`
+		`<tr><td>Details</td><td>`, result[1], `</td></tr>`
 	//	`<tr><td>Build log</td><td><pre>`, tryReadText(testDir ~ "build.log").encodeEntities(), `</pre></td></tr>`
 		`<tr><td>Build log</td><td><a href="build.log">View</a></td></tr>`
-		`<tr><td>Diff stat</td><td><pre>`, tryReadText(testDir ~ "diffstat.ansi").encodeEntities().ansiToHtml(), `</pre></td></tr>`
+		`<tr><td>Files</td><td><a href="file/web/index.html">Index</a> &middot; <a href="file/">List</a></td></tr>`
+	);
+	if (result[0] == "success" && exists(testDir ~ "numstat.txt"))
+	{
+		auto lines = readText(testDir ~ "numstat.txt").strip.splitLines.map!(line => line.split('\t')).array;
+		if (!lines.length)
+			html.put(`(no changes)`);
+		int additions, deletions, maxChanges;
+		foreach (line; lines)
+		{
+			if (line[0] == "-")
+				additions++, deletions++;
+			else
+			{
+				additions += line[0].to!int;
+				deletions += line[1].to!int;
+				maxChanges = max(maxChanges, line[0].to!int + line[1].to!int);
+			}
+		}
+
+		html.put(
+			`<tr><td>Changes</td><td>`
+			`<table class="changes">`
+		);
+		auto changeWidth = min(100.0 / maxChanges, 5.0);
+		foreach (line; lines)
+		{
+			auto fn = line[2];
+			html.put(`<tr><td>`, encodeEntities(fn), `</td><td>`);
+			if (line[0] == "-")
+				html.put(`(binary file)`);
+			else
+			{
+				html.put(`<div class="additions" style="width:%5.3f%%" title="%s addition%s"></div>`.format(line[0].to!int * changeWidth, line[0], line[0]=="1" ? "" : "s"));
+				html.put(`<div class="deletions" style="width:%5.3f%%" title="%s deletion%s"></div>`.format(line[0].to!int * changeWidth, line[0], line[0]=="1" ? "" : "s"));
+			}
+			html.put(
+				`</td>`
+				`<td>`
+					`<a href="file/`, encodeEntities(fn), `">Old</a> `
+					`<a href="../!base/file/`, encodeEntities(fn), `">New</a> `
+					`<a href="diff/`, encodeEntities(fn), `">Diff</a>`
+				`</td>`
+				`</tr>`
+			);
+		}
+		html.put(
+			`</table>`
+			`</td></tr>`
+		);
+	}
+	html.put(
 		`</table>`
 	);
 }
@@ -132,6 +257,9 @@ shared static this()
 void main()
 {
 	log = createLogger("WebServer");
+
+	cache = Repository("work/cache-git/v2/");
+	objectReader = cache.createObjectReader();
 
 	auto server = new HttpServer();
 	server.log = log;
