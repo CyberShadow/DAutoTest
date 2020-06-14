1d069bf367fd626ebcc3bafa95da304ecae42748
Vladimir Panteleev <git@thecybershadow.net>
2020-06-14 13:59:12 +0000

    autotest: Configure the GitHub API token for ae.sys.d.manager too

--- autotest.d
+++ autotest.d
@@ -58,6 +58,7 @@ void main()
 	d.config.local.workDir = "work".absolutePath();
 	d.config.local.cache = "git";
 	d.config.local.timeout = config.timeout;
+	d.config.local.githubToken = config.token;
 	d.config.build.environment = config.env.dup.byPair.assocArray;
 	d.autoClean = true;
 
